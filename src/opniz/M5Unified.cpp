a3344d65273f4169fae040105a201be4885093a9
Miso Tanaka <miso.develop@gmail.com>
2023-10-10 21:10:43 +0900

    🎉Release v0.2.0

--- src/opniz/M5Unified.cpp
+++ src/opniz/M5Unified.cpp
@@ -131,7 +131,7 @@ void Opniz::M5Unified::_constructor() {
 
 
 String Opniz::M5Unified::_getVersion______String_Handler::procedure(JsonArray params) {
-    return "OpnizM5Unified@0.1.1";
+    return "OpnizM5Unified@0.2.0";
 }
 
 
